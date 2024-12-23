2a3dc89966bd741275656c81d07d5e0e5db887d5
Amr224 <72498648+Amr224@users.noreply.github.com>
2024-12-23 19:30:06 +0200

    Initial Routing Setup, Layout & components placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import { useState } from 'react'
 import './App.css'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+
+// Pages
+import AllNotes from './Pages/AllNotes.jsx'
+import ArchivedNotes from './Pages/ArchivedNotes.jsx'
+import NotFound from './Pages/NotFound.jsx'
+// Layouts
+import Layout from './Layout/Layout.jsx'
 
 function App() {
-
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route index element={<AllNotes/>}></Route>
+      <Route path='/ArchivedNotes' element={<ArchivedNotes/>}></Route>
+      <Route path='*' element={<NotFound/>}/>
+    </Route>
+  ))
   return (
     <>
+    <RouterProvider router={router}/>
     <h1>All Notes</h1>
     </>
   )
---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+const Layout = () => {
+  return (
+    <div>Layout</div>
+  )
+}
+
+export default Layout
