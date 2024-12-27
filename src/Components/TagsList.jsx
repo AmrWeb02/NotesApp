0169c42528452a7e9031a2f4cb80ca2cc24bd622
Amr224 <72498648+Amr224@users.noreply.github.com>
2024-12-27 22:36:04 +0200

    Tags in sidebar are now fetched dynamically isntead of being hardcoded

--- src/Components/TagsList.jsx
+++ src/Components/TagsList.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaTag } from "react-icons/fa";
 
 const TagsList = () => {
+    let fetchedArray;
+    const [TagList, setTagList] = useState([]);
+    useEffect( () => {
+        async function fetchTags(){
+        const res = await fetch('http://localhost:4000/notes');
+        const tags = await res.json();
+        fetchedArray = tags.map((note)=>{return note.tag});
+        setTagList(fetchedArray);
+        }
+        fetchTags();
+
+    }, [])
+    
   return (
    <>
    <div style={{paddingLeft:"22px"}}>
    <h3 style={{paddingTop:"30px", color: "#ffffff",}}>Tags</h3>
    <ul>
-    <li className="tag-item">
-        <button className="tag-btn"><FaTag/>Exercise</button>
-    </li>
-    <li className="tag-item">
-        <button className="tag-btn"><FaTag/>Cooking</button>
-    </li>
-    <li className="tag-item">
-        <button className="tag-btn"><FaTag/>Study</button>
-    </li>
-    <li className="tag-item">
-        <button className="tag-btn"><FaTag/>Exams</button>
-    </li>
+    { TagList.map((tag)=>{return     <li key={tag} className="tag-item">
+        <button className="tag-btn"><FaTag/>{tag}</button>
+    </li> })}
+
    </ul>
    </div>
    </>
